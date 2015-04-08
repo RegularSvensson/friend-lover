52a1beae3f27689b1101f9921f543c0b59ab4e53
RegularSvensson <elias.svensson.1992@gmail.com>
2015-04-08 16:57:36 +0200

    Add moddel object to hold current friend & array of friend objects

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
-app.js
+/* ======= Model ======= */
+
+var model = {
+	currentFriend: null,
+	friend: [
+		{
+			clickCount: 0,
+			name: 'Meron',
+			imgSrc: 'img/meron.jpg'
+		},
+		{
+			clickCount: 0,
+			name: 'Isak',
+			imgSrc: 'img/isak.jpg'
+		},
+		{
+			clickCount: 0,
+			name: 'Robban',
+			imgSrc: 'img/robban.jpg'
+		},
+		{
+			clickCount: 0,
+			name: 'Tim',
+			imgSrc: 'img/tim.jpg'
+		},
+		{
+			clickCount: 0,
+			name: 'Pontus',
+			imgSrc: 'img/pontus.jpg'
+		}
+	]
+};
+
